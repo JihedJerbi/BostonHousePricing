bc777f21463a692512cfe686a6f25ee2c47e3421
jihed jerbi <jihed.eljerbi@gmail.com>
2022-10-19 00:06:51 +0100

    This is the commit for front application

--- app.py
+++ app.py
@@ -26,10 +26,13 @@ def predict_api():
 @app.route('/predict',methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]
+    print(data)
     final_input=scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=regmodel.predict(final_input)[0]
     return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+
+
 
 
 
